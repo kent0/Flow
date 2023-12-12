0b3a11f8382d3cf030dde4ea8ed8902d5b909ecc
Kento Kaneko <kaneko@mit.edu>
2023-12-12 16:56:40 -0500

    declare heaviside_ to avoid heaviside from toolbox

--- legacy/problem.m
+++ legacy/problem.m
@@ -1,5 +1,7 @@
 function [ufun,f,dom,ifexact]=problem(cname,nu,Ny)
 
+heaviside_ = @(x) [0.5 * (x == 0) + (x > 0)];
+
 if strcmp(cname,'st2')
     ufun=@(x,y,t) st2_uv(x,y,t,w,nu);
     dom=[0,1,0,1];
@@ -24,12 +26,12 @@ elseif strcmp(cname,'walsh')
     ifexact=true;
 elseif strcmp(cname,'ldc')
     ufun=@(x,y,t) [y.^(32);y*0];
-    ufun=@(x,y,t) [heaviside(y-1+1.e-3);y*0];
+    ufun=@(x,y,t) [heaviside_(y-1+1.e-3);y*0];
     dom=[0,1,0,1];
     f=@(x,y) [x;y]*0;
     ifexact=false;
 elseif strcmp(cname,'ldc_reg')
-    ufun=@(x,y,t) [16*heaviside(y-1+1.e-3).*(x.*(1-x)).^2;y*0];%ldc_reg_uv(x,y,t);
+    ufun=@(x,y,t) [16*heaviside_(y-1+1.e-3).*(x.*(1-x)).^2;y*0];%ldc_reg_uv(x,y,t);
 %   dom=[-1,1,-1,1];
     dom=[0,1,0,1];
     f=@(x,y) [x;y]*0;
@@ -51,3 +53,4 @@ elseif strcmp(cname,'decay2')
     f=@(x,y) [x;x]*0;
     ifexact=true;
 end
+
